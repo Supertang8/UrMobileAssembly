f81567bb94a5e894624b1cd48e9db89c2753cca7
Jacob Slot <jslot23@student.aau.dk>
2024-04-16 09:33:08 +0200

    bare endnu en update

---
+++ GUI/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Define a list to store order list data
+order_list = []
+print("test")
+
+# Route to render the index.html template
+@app.route('/')
+def index():
+    return render_template('index.html', order_list=order_list)
+
+@app.route('/receive_order_list', methods=['POST'])
+def receive_order_list():
+    order_list_data = request.json
+    # Process the order list data as needed
+    print(order_list_data)
+    return jsonify({'message': 'Data received successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
