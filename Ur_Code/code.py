7e22f4b0a7b95fb68c618b7a9b4010e373593a00
Supertang8 <stang23@student.aau.dk>
2024-05-17 11:16:54 +0200

    error

--- Ur_Code/code.py
+++ Ur_Code/code.py
@@ -162,7 +162,7 @@ while True:
             #power_log_file = open("power_log.txt", 'w')
             #power_log_file.write('Time[s] Voltage[V] Current[A] Power[W]\n')
             assembly_log_file.write('Order Completion_time[s]')
-            joint_pos_log_file.write('Time[s] Joint_pos Joint_vel Joint_acc')
+            joint_pos_log_file.write('Time[s] actual_joint_pos actual_joint_vel target_joint_pos target_joint_vel target_joint_acc')
             waiting_for_order_printed = False
             print(f'Order(s) received: {orders}')
             paused = False
@@ -199,7 +199,7 @@ while True:
         else:
             # log the power and values
             #power_log_file.write(f'{time.time()-start_time} {state.actual_robot_voltage} {state.actual_robot_current} {state.actual_robot_voltage*state.actual_robot_current}\n')
-            joint_pos_log_file.write(f'{time.time()-start_time} {state.actual_q} {state.actual_qd} {state.actual_qdd}')
+            joint_pos_log_file.write(f'{time.time()-start_time} {state.actual_q} {state.actual_qd} {state.target_q} {state.target_qd} {state.target_qdd}')
 
             #Check if the queue has been finished.
             if current_task >= len(queue):
